1bb4e2035efd2acd75a9f8ba1a625a58dfdac40f
Abhinav Agarwal <114795309+AbhinavAgarwal1111@users.noreply.github.com>
2022-10-03 19:50:32 +0530

    Day3 Progress

---
+++ Day3/solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+
+public class solution {
+    public static void main(String[] arg){
+    Scanner ob= new Scanner(System.in);
+    int N=ob.nextInt();
+    for(int i=1;i<=10;i++){
+        System.out.println(+N+ " x " +i+ " = " +N*i);
+    }
+}
+}
