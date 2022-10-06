731256e38441e95ca15f3080485f4d7a5fb4bf87
Abhinav Agarwal <114795309+AbhinavAgarwal1111@users.noreply.github.com>
2022-10-06 21:37:59 +0530

    Day6 Progress
    Topic of the day:Java Datatypes
    Java has 8 primitive data types; char, boolean, byte, short, int, long, float, and double. For this exercise, we'll work with the primitives used to hold integer values (byte, short, int, and long):
    A byte is an 8-bit signed integer.
    A short is a 16-bit signed integer.
    An int is a 32-bit signed integer.
    A long is a 64-bit signed integer.
    Question: Given an input integer, you must determine which primitive data types are capable of properly storing that input.
    
    Input Format:
    The first line contains an integer, T, denoting the number of test cases.
    Each test case,T , is comprised of a single line with an integer, n , which can be arbitrarily large or small.
    Test case 1:
    For each input variable n  and appropriate primitive datatypes, you must determine if the given primitives are capable of storing it.
    Output:
    n can be fitted in:
    * dataType
    Test case 2:
    If there is more than one appropriate data type, print each one on its own line and order them by size (i.e.:byte<short<int<long).
    Output:
    n can't be fitted anywhere.
    Sample Input
    
    -123
    150000
    27938498492 
    3568888888888888888888888888
    -13444444444333
    
    Sample Output
    
    -123 can be fitted in:
    * short
    * int
    * long
    150000 can be fitted in:
    * int
    * long
    27938498492 can be fitted in:
    * int
    * long
    3568888888888888888888888888 can't be fitted anywhere.
    -13444444444333 can be fitted in:
    * long

---
+++ Day6 Java Datatypes/solution.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class solution {
+
+    public static void main(String[] args) 
+        {
+        Scanner ob = new Scanner(System.in);
+        int t= ob.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            try
+            {
+                long x=ob.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                 if (x>=-32768 && x<=32767)System.out.println("* short");
+                 if (x>=-2147483648 && x<=2147483647)System.out.println("* int");
+                 if (x>=-9223372036854775808L && x<=9223372036854775807L)System.out.println("* long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(ob.next()+" can't be fitted anywhere.");
+            }
+        }
+    }
+}
