8ecec1d1d02b314f16c0cac5939f872b8527bb9c
Abhinav Agarwal <114795309+AbhinavAgarwal1111@users.noreply.github.com>
2022-10-17 21:18:04 +0530

    Day17 Progress
    Topic of the Day: Java SubArray
    A subarray of an -element array is an array composed from a contiguous block of the original array's elements.
    The sum of an array is the total sum of its elements.
    An array's sum is negative if the total sum of its elements is negative.
    An array's sum is positive if the total sum of its elements is positive.
    Input Format
    The first line contains a single integer,n , denoting the length of array A=[a0,a1,.....,an-1].
    The second line contains n space-separated integers describing each respective element,ai , in array A.
    
    Output Format
    Print the number of subarrays of A having negative sums.
    
    Input
    5
    1 -2 4 -5 1
    
    Output
    9

---
+++ Day0017 Java SubArray/solution.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class solution {
+    public static void main(String[] args) {
+
+        int n,sum=0,c=0;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                sum = 0;
+                for(int k=i;k<=j;k++)
+                {
+                    sum+=a[k];
+                   
+                }
+                 if(sum<0)c++;
+            }
+        }
+        System.out.print(c);
+    }
+    
+}
