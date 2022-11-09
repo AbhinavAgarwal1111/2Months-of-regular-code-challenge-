5d02e33a6daeecc34920a453b5964782aa863355
Abhinav Agarwal <abhinavagarwal9999@gmail.com>
2022-11-09 18:16:36 +0530

    Day39 Progress
    Topic of the Day: Java Custom Sort String
    Problem: We are given two strings order and s. All the characters of order are unique and were sorted in some custom order previously. Permute the characters of s so that they match the order that order was sorted. More specifically, if a character x occurs before a character y in order, then x should occur before y in the permuted string. Return any permutation of s that satisfies this property.
     
    Example 1:
    Input: order = "cba", s = "abcd"
    Output: "cbad"
    Explanation: 
    "a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a". 
    Since "d" does not appear in order, it can be at any position in the returned string. "dcba", "cdba", "cbda" are also valid outputs.
    
    Example 2:
    Input: order = "cbafg", s = "abcd"
    Output: "cbad"
    
    Testcase:
     Your input
    "cba"
    "abcd"
    "cbafg"
    "abcd"
    Output
    "cbad"
    "cbad"
    Expected
    "cbad"
    "cbad"

---
+++ Day0039 Java Custom Sort String/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+         public String customSortString(String order, String s) {
+    Map<Character, Integer> frequency = new HashMap<>();
+    for (char c : s.toCharArray()) {
+      frequency.put(c, frequency.getOrDefault(c, 0) + 1);
+    }
+    StringBuilder sb = new StringBuilder();
+    for (char c : order.toCharArray()) {
+      int count = frequency.getOrDefault(c, 0);
+      while (count-- > 0) {
+        sb.append(c);
+      }
+      frequency.put(c, 0);
+    }
+    for (Character key : frequency.keySet()) {
+      int count = frequency.getOrDefault(key, 0);
+      while (count-- > 0) {
+        sb.append(key);
+      }
+    }
+    return sb.toString();
+  }
+}
