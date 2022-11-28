673b7480b8999d0c50400e868cc55d8896414421
Abhinav Agarwal <abhinavagarwal9999@gmail.com>
2022-11-28 20:54:16 +0530

    Day58 Progress
    Topic of the day: Java Binary Tree Traversal 
    PROBLEM: Given the root of a binary tree, return the inorder traversal of its nodes' values.
    
    TESTCASES:
    CASE 1:
    Input: root = [1,null,2,3]
    Output: [1,3,2]
    
    CASE 2:
    Input: root = []
    Output: []
    
    CASE 3:
    Input: root = [1]
    Output: [1]
    
    EXPLANATION
    Inorder traversal using recursion:
    Step 1: Recursively traverse the left subtree  
    Step 2: Now, visit the root  
    Step 3: Traverse the right subtree recursively  
    
     INORDER TRAVERSAL
    INPUT: 1 2 3 4 5
    LOGIC: (Left, Root, Right) : 4 2 5 1 3

---
+++ Day0058 Java Binary Tree Traversal/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            list.add(stack.peek().val);
+            node = stack.pop().right;
+        }
+        return list;
+    }
+}
